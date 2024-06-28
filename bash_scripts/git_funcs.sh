c853f29e497fecacfa6e7651a6c3190e735620a8
Tak <tkmogel@gmail.com>
2024-06-28 12:17:09 -0400

    fixed some outdate git commands

--- bash_scripts/git_funcs.sh
+++ bash_scripts/git_funcs.sh
@@ -8,18 +8,20 @@ co = checkout
 brn = symbolic-ref --short HEAD
 # ”status” has too many letters in it
 sts = status
+#commit with message
+cm = "!sh -c 'git commit -m \"$1\"' -"
 # if you use a JIRA ticket naming strategy (eg PROJ-12345-branch-name) to name your
 # branches, this will automatically prepend the comment with the ticket
-cm = !"f () { branch=$(git brn | cut -d '-' -f-2); msg=$1; git commit -m \"${branch}: ${msg}\"; unset branch; unset msg;}; f"
+cmb = !"f () { branch=$(git brn | cut -d '-' -f-2); msg=$1; git commit -m \"${branch}: ${msg}\"; unset branch; unset msg;}; f"
 # (new push) If you  need the first push to set upstream, just np and kick back
 np = !"f () { branch=$(git brn); git push --set-upstream origin ${branch}; unset branch; }; f"
 # create and checkout new branch
 new = !sh -c 'git co -b $1 ' $1
-# commit / add all. Use at your own peril
+# add/commit all. Use at your own peril
 cma = !sh -c 'git add . && git cm "$1" ' $1
 # overwrite the file with the version at master. Again, peril.
-mfile = !sh -c 'git checkout origin/master -- "$1" ' $1
-# open the gui. This isn’t always configured by default. And your exe might be
-# in another location. You can probably skip this one
-gui = !sh -c '/usr/libexec/git-core/git-gui'
+mfile = !sh -c 'git checkout origin/main -- "$1" ' $1
+# open the gui. This isn’t always configured by default.
+# to find the location of your git-gui use "whereis git-gui"
+gui = !sh -c '/usr/share/git-gui'
 
